cdb14e7949027c068940e7839028d0396110417a
ASUS <ASUS@DESKTOP-DV9PLLJ>
2019-07-13 10:19:28 +0800

    页面静态化

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from 'react-snapshot';
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-ReactDOM.render(
+render(
     <Router>
         <Route path="/" component={App} />
     </Router>,
